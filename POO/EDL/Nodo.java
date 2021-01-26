f028837b181425a3a0e2d3773634e8974a1bfe1d
HuberM1998 <huber.mathaus@gmail.com>
2021-01-26 18:21:17 -0300

    Estrutura de dados lineares, em orientação a objetos

---
+++ POO/EDL/Nodo.java
@@ -0,0 +1,16 @@
+public class Nodo {
+	
+    private int idade;
+
+    public Nodo(int idade) {
+        this.idade = idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+}
