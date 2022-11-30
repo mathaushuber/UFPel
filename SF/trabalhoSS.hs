8cda2895091500b62f645550e6a7ad1c01522401
HuberM1998 <huber.mathaus@gmail.com>
2022-11-30 18:18:15 -0300

    Trabalho Small-Step

--- SF/trabalhoSS.hs
+++ SF/trabalhoSS.hs
@@ -17,12 +17,13 @@ data B = TRUE
    deriving(Eq,Show)
 
 data C =    While B C
-     | DoWhile C B -- Do C while B
+     | DoWhile B C
      | If B C C
      | Seq C C
      | Atrib E E
      | Skip
-   deriving(Eq,Show)                
+   deriving(Eq,Show)    
+            
 
 -----------------------------------------------------
 -----
@@ -35,24 +36,30 @@ data C =    While B C
 --- primeiro elemento da tupla é uma String (nome da variável) e o segundo um Inteiro
 --- (conteúdo da variável):
 
-
 type Memoria = [(String,Int)]
 
+--  Memórias 
 exSigma :: Memoria
 exSigma = [ ("x", 10), ("temp",0), ("y",0)]
 exSigma2 :: Memoria
-exSigma2 = [("x",0), ("temp",1), ("y",0)]
--- exSigma3 :: Memoria
--- exSigma3 = [("x", TRUE), ("temp", FALSE), ("y", TRUE)]
+exSigma2 = [("x",10), ("y",1)]
+exSigma3 :: Memoria
+exSigma3 = [ ("x", 0), ("y",5), ("z",7)]
 exSigma4 :: Memoria
-exSigma4 = [("x",10), ("temp",1)]
-
+exSigma4 = [ ("x", 4), ("y",10), ("z",3)]
+exSigma5 :: Memoria
+exSigma5 = [ ("x", 5), ("y", 22), ("z",15)]
+exSigma6 :: Memoria
+exSigma6 = [ ("x", 7), ("temp",15), ("y",0)]
+exSigma7 :: Memoria
+exSigma7 = [ ("x", 2), ("temp",30), ("y",47)]
+exSigma8 :: Memoria
+exSigma8 = [ ("x", 8), ("temp",102), ("y",79)]
 --- A função procuraVar recebe uma memória, o nome de uma variável e retorna o conteúdo
 --- dessa variável na memória. Exemplo:
 ---
 --- *Main> procuraVar exSigma "x"
 --- 10
-
 
 procuraVar :: Memoria -> String -> Int
 procuraVar [] s = error ("Variavel " ++ s ++ " nao definida no estado")
@@ -60,13 +67,12 @@ procuraVar ((s,i):xs) v
   | s == v     = i
   | otherwise  = procuraVar xs v
 
-
 --- A função mudaVar, recebe uma memória, o nome de uma variável e um novo conteúdo para essa
 --- variável e devolve uma nova memória modificada com a varíável contendo o novo conteúdo. A
 --- chamada
 ---
 --- *Main> mudaVar exSigma "temp" 20
---- [("x",10),("temp",20),("temp",0)]
+--- [("x",10),("temp",20),("y",0)]
 ---
 ---
 --- essa chamada é equivalente a operação exSigma[temp->20]
@@ -78,29 +84,23 @@ mudaVar ((s,i):xs) v n
   | otherwise  = (s,i): mudaVar xs v n
 
 
--------------------------------------
----
---- Completar os casos comentados das seguintes funções:
----
----------------------------------
-
 smallStepE :: (E,Memoria) -> (E,Memoria)
 -- variavel
 smallStepE (Var x,s) = (Num (procuraVar s x),s)
 -- soma
-smallStepE (Soma (Num x) (Num temp), s) = (Num (x+temp),s)
+smallStepE (Soma (Num x) (Num y), s) = (Num (x+y),s)
 smallStepE (Soma (Num x) e2, s) = let (ef,_) = smallStepE (e2 ,s)
                                  in (Soma (Num x) ef,s)
 smallStepE (Soma e1 e2,s)  = let (ef,_) = smallStepE (e1, s)
                             in (Soma ef e2,s)
 --subtração
-smallStepE (Sub (Num x) (Num temp), s) = (Num (x-temp),s)
+smallStepE (Sub (Num x) (Num y), s) = (Num (x-y),s)
 smallStepE (Sub (Num x) e2, s) = let (ef,_) = smallStepE (e2 ,s)
                                  in (Sub (Num x) ef,s)
 smallStepE (Sub e1 e2,s)  = let (ef,_) = smallStepE (e1, s)
                             in (Sub ef e2,s)
 --multiplicação
-smallStepE (Mult (Num x) (Num temp), s) = (Num (x*temp),s)
+smallStepE (Mult (Num x) (Num y), s) = (Num (x*y),s)
 smallStepE (Mult (Num x) e2, s) = let (ef,_) = smallStepE (e2 ,s)
                                  in (Mult (Num x) ef,s)
 smallStepE (Mult e1 e2,s)  = let (ef,_) = smallStepE (e1, s)
@@ -127,14 +127,14 @@ smallStepB (Or b1 b2,s)    = let (bn,sn) = smallStepB (b1,s)
                               in (Or bn b2,sn)
 
 --IGUAL
-smallStepB (Igual (Num x) (Num temp), s) = if (x==temp) then (TRUE,s) else (FALSE, s)
+smallStepB (Igual (Num x) (Num y), s) = if (x==y) then (TRUE,s) else (FALSE, s)
 smallStepB (Igual (Num x) e2, s) = let (el,_) = smallStepE (e2 ,s)
                                  in (Igual (Num x) el,s)
 smallStepB (Igual e1 e2,s)  = let (el,_) = smallStepE (e1, s)
                             in (Igual el e2,s)
 
 --MENOR
-smallStepB (Leq (Num x) (Num temp), s) = if (x<temp) then (TRUE,s) else (FALSE, s)
+smallStepB (Leq (Num x) (Num y), s) = if (x<y) then (TRUE,s) else (FALSE, s)
 smallStepB (Leq (Num x) e2, s) = let (el,_) = smallStepE (e2 ,s)
                                  in (Leq (Num x) el,s)
 smallStepB (Leq e1 e2,s)  = let (el,_) = smallStepE (e1, s)
@@ -160,80 +160,128 @@ smallStepC (Seq c1 c2,s) = let(cl,sn) = smallStepC(c1,s)
 smallStepC (While b c, s) = ((If b (Seq c (While b c)) Skip), s)
 
 --doWhile
-smallStepC (DoWhile c b, s) = (Seq b (While c b), s)
+smallStepC (DoWhile b c, s) = (Seq c (While b c), s)
 
 ----------------------
 --  INTERPRETADORES
 ----------------------
+
 
 --- Interpretador para Expressões Aritméticas:
 isFinalE :: E -> Bool
 isFinalE (Num a) = True
 isFinalE _       = False
 
+
 interpretadorE :: (E,Memoria) -> (E,Memoria)
 interpretadorE (a,s) = if isFinalE a then (a,s) else interpretadorE (smallStepE (a,s))
 
 --- Interpretador para expressões booleanas
 isFinalB :: B -> Bool
-isFinalB TRUE  = True
-isFinalB FALSE = True
-isFinalB _     = False
-
+isFinalB TRUE   = True
+isFinalB FALSE  = True
+isFinalB _      = False
 
 interpretadorB :: (B,Memoria) -> (B,Memoria)
 interpretadorB (b,s) = if isFinalB b then (b,s) else interpretadorB (smallStepB (b,s))
 
 -- Interpretador da Linguagem Imperativa
 isFinalC :: C -> Bool
-isFinalC Skip = True
-isFinalC _    = False
+isFinalC Skip   = True
+isFinalC _      = False
 
 interpretadorC :: (C,Memoria) -> (C,Memoria)
 interpretadorC (c,s) = if isFinalC c then (c,s) else interpretadorC (smallStepC (c,s))
 
-
-
 exemplo :: E
 exemplo = Soma (Num 3) (Soma (Var "x") (Var "temp"))
+-- interpretadorE(exemplo, exSigma) 
+-- -*>
+-- (Num 13,[("x",10),("temp",0),("y",0)])
 exemplo3 :: E
 exemplo3 = Sub (Num 3) (Sub (Var "x") (Var "temp"))
+-- interpretadorE(exemplo3, exSigma)
+-- -*>
+-- (Num (-7),[("x",10),("temp",0),("y",0)])
 exemplo4 :: E
 exemplo4 = Mult (Num 3) (Mult (Var "x") (Var "temp"))
+-- interpretadorE(exemplo4, exSigma)
+-- -*>     
+-- (Num 0,[("x",10),("temp",0),("y",0)])
 
 exemploAtrib :: C
-exemploAtrib = Atrib (Var "x") (Var "temp")
-
--- RODANDO O EXEMPLO:
--- Hugs> interpretadorE (exemplo, exSigma2)
+exemploAtrib = Atrib (Var "x") (Var "y")
+-- interpretadorC(exemploAtrib, exSigma3)
+-- -*>
+-- (Skip,[("x",5),("y",5),("z",7)])
 
 testeNovo :: C
 testeNovo = (While (Not (Igual (Var "x") (Num 3))) (Seq (Atrib (Var "x") 
-                              (Soma (Var "x") (Num 1))) (Atrib (Var "temp") (Soma (Var "temp") (Num 3)))) )
+                              (Soma (Var "x") (Num 1))) (Atrib (Var "y") (Soma (Var "y") (Num 3)))) )
+-- while(x != 3){
+--    x = x + 1;
+--    y = y + 3;   
+-- }
+-- interpretadorC(testeNovo, exSigma3)
+-- -*>
+-- (Skip,[("x",3),("y",14),("z",7)])
+-- Se usar um x > 3 entra em loop.
 
 exemplo2 :: B
 exemplo2 = And (And TRUE (Not FALSE)) (And (Not (Not TRUE)) TRUE)
-
+--  interpretadorB(exemplo2, exSigma)
+-- -*>
+-- (TRUE,[("x",10),("temp",0),("y",0)])
 exemploOR :: B
 exemploOR = Or FALSE (And TRUE (Not FALSE)) 
--- *Main> interpretadorB (exemplo2,exSigma2)
--- (TRUE,[("x",3),("temp",0),("y",0)])
+-- *Main> interpretadorB (exemplo2,meuEstado)
+-- (TRUE,[("x",3),("y",0),("z",0)])
 
 --- TESTES
 
-testec1 :: C
-testec1 = (Seq (Seq (Atrib (Var "y") (Var "x")) (Atrib (Var "x") (Var "temp")))(Atrib (Var "temp") (Var "y")))
-
+testeSeq :: C
+testeSeq = (Seq (Seq (Atrib (Var "z") (Var "x")) (Atrib (Var "x") (Var "y")))(Atrib (Var "y") (Var "z")))
+-- exSigma3 = [ ("x", 2), ("y",5), ("z",7)]
+-- z = x
+-- x = y
+-- y = z
+-- interpretadorC(testeSeq, exSigma3)
+-- -*>
+-- (Skip,[("x",5),("y",2),("z",2)])
 fatorial :: C
-fatorial = (Seq (Atrib (Var "temp") (Num 1))
+fatorial = (Seq (Atrib (Var "y") (Num 1))
                 (While (Not (Igual (Var "x") (Num 1)))
-                       (Seq (Atrib (Var "temp") (Mult (Var "temp") (Var "x")))
+                       (Seq (Atrib (Var "y") (Mult (Var "y") (Var "x")))
                             (Atrib (Var "x") (Sub (Var "x") (Num 1))))))
+-- interpretadorC(fatorial, exSigma)                                                        
+-- -*>
+-- (Skip,[("x",1),("temp",0),("y",3628800)
 
+testeLeq :: B 
+testeLeq = (Leq (Var "x") (Num 10))
+-- interpretadorB(testeLeq, exSigma)
+-- -*>
+-- (FALSE,[("x",10),("temp",0),("y",0)])
 
-menor1 :: B 
-menor1 = (Leq (Var "x") (Num 10))
-
-exercicio :: C
-exercicio = (Seq (Atrib (Var "x") (Num 0))
+exemploTeste :: C
+exemploTeste = (Seq (Atrib (Var "x") (Num 0))
                   (While (Leq (Var "x") (Num 10)) ( Atrib (Var "x") (Soma (Var "x") (Num 1)))))
+-- x = 0;
+-- while (x <= 10)
+-- x += 1;
+-- interpretadorC(exemploTeste, exSigma2)
+-- -*>
+--(Skip,[("x",10),("temp",0),("y",0)])
+
+programaUm :: C
+programaUm = (If (Leq (Var "x") (Var "y")) 
+                (Atrib (Var "temp") (Mult (Num 2) (Num 2))) 
+                  (Atrib (Var "temp") (Num 5)))
+--Primeiro comparamos se a variável x <= y, depois atribuimos à variável temp = 2*2, por fim atribuimos à variável temp = 5
+--		if(x <= y){
+--			temp = 2*2;
+--			temp = 5;
+--		}
+-- interpretadorC(programaUm, exSigma)
+-- -*>
+-- (Skip,[("x",10),("temp",5),("y",0)])
