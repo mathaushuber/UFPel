b525eb163ecb28aa056312b256b0173cfed85519
HuberM1998 <huber.mathaus@gmail.com>
2023-09-17 00:18:49 -0300

    feat: nova implementação do compilador

---
+++ PCOMP/CompiladorFinal/GeneratedOutputs/exemplo1_output_generator.java
@@ -0,0 +1,11 @@
+package org.karloff.output;
+import java.util.Scanner;
+public class KarloffOutputGenerator {
+
+public static void main(String[] args) {
+int teste;
+teste = 9;
+System.out.println(teste);
+}
+
+}
